fc84fc4ac1fdd2d7f303e62af6c5c3dafecc9c28
yzq <739291773@qq.com>
2018-11-30 16:38:44 +0800

    从头实现一次，并代理method到vm中

--- step1.js
+++ step1.js
@@ -33,3 +33,36 @@ obj.sex = 'woman'
 
 console.log(obj.sex) 
 
+// for(let data of obj){
+//     console.log(data)
+// }
+
+// for(let i in obj){
+//     console.log(obj[i])
+// }
+
+// array.forEach(element => {
+//     console.log(element)
+// });
+// forEach： 数组  不能被break，continue中断
+// for in： 对象的键（数组）  性能差，原型链也会遍历，顺序随机（不适合数组，适合稀疏数组的遍历）
+// for of： 数组的值 解决上面所有问题，不能遍历普通对象
+
+let arr1 = [1,2,0]
+let arr2 = [3,4]
+// let tmp = arr1.concat(arr2)  不改变原数组
+// let tmp = arr1.join('?')   不改变原数组
+
+// let tmp = arr1.pop()  改变原数组
+// let tmp = arr1.push(3)  改变原数组
+// let tmp = arr1.unshift(0)  改变原数组
+// let tmp = arr1.shift() 改变原数组
+// let tmp = arr1.reverse()   改变原数组
+// let tmp = arr1.sort()  改变原数组
+let tmp = arr1.splice(1,1,5)  //改变原数组
+
+console.log(tmp)
+console.log(arr1)
+console.log(arr2)
+
+
